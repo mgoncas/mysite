a202d571face0d46a42d22fd7b7850f6569e4614
Miguel González Castro <mgcastro86@gmail.com>
2015-08-09 21:46:04 +0200

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,6 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
-    #prueba comentario
     was_published_recently.short_description = 'Published recently?'
 
 
