8ea33653761b663cc9b775fe03a983e5d738e483
mgoncas <mgcastro86@gmail.com>
2015-08-09 21:36:55 +0200

    Segunda parte del tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # ordenamos los campos a nuestro antojo
+    # fields = ['pub_date', 'question_text']
+
+    # dividir form en fieldsets
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # agregamos la gestion de Choice dentro de Question
+    inlines = [ChoiceInline]
+    # definimos las columnas que queremos que aparezcan en su listado en el admin
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text', 'pub_date']
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
+# agregar Choice, django solo detecta que estan relacionados
+# admin.site.register(Choice)
